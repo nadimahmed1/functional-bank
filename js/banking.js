ce630901201da98706a35c997780773b4bb9637e
nadimahmed1 <nadimarafat26@gmail.com>
2022-06-01 13:17:00 +0600

    functional withdraw button added

--- js/banking.js
+++ js/banking.js
@@ -5,13 +5,13 @@
 // const first = dubleIt(5);
 // const second = dubleIt(7);
 
-function getInputValue() {
-    const depositInput = document.getElementById('deposit-input');
-    const depositInputText = depositInput.value;
-    const previousInputText = parseFloat(depositInputText);
+function getInputValue(inputId) {
+    const inputField = document.getElementById(inputId);
+    const inputAmountText = inputField.value;
+    const amountValue = parseFloat(inputAmountText);
     // clear  deposit input field
-    depositInput.value = '';
-    return previousInputText;
+    inputField.value = '';
+    return amountValue;
 
 }
 
@@ -19,7 +19,7 @@ document.getElementById('deposit-button').addEventListener('click', function () 
     // const depositInput = document.getElementById('deposit-input');
     // const depositInputText = depositInput.value;
     // const previousInputText = parseFloat(depositInputText);
-    const previousInputText = getInputValue();
+    const previousInputText = getInputValue('deposit-input');
     // get current deposit
 
     const depositTotal = document.getElementById('deposit-total');
@@ -40,17 +40,17 @@ document.getElementById('deposit-button').addEventListener('click', function () 
 })
 // get the withdraw
 document.getElementById('withdraw-button').addEventListener('click', function () {
-    const withdrawInput = document.getElementById('withdraw-input');
-    const withdrawInputText = withdrawInput.value;
-    const PreviousWithdrawAmount = parseFloat(withdrawInputText);
-    // 
+    // const withdrawInput = document.getElementById('withdraw-input');
+    // const withdrawInputText = withdrawInput.value;
+    // const PreviousWithdrawAmount = parseFloat(withdrawInputText);
+    const PreviousWithdrawAmount = getInputValue('withdraw-input');
+    // // 
     const withdrawTotal = document.getElementById('withdraw-total');
     const withdrawTotalText = withdrawTotal.innerText;
     const PreviousWithdrawTotal = parseFloat(withdrawTotalText);
     const newWithdrawAmount = PreviousWithdrawTotal + PreviousWithdrawAmount;
     withdrawTotal.innerText = newWithdrawAmount;
-    // clear withdraw input field
-    withdrawInput.value = '';
+
     // update balance before withdraw
     const balanceTotal = document.getElementById('balance-total');
     const balanceTotalText = balanceTotal.innerText;
